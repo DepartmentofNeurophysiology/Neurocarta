2989780efe3c814a01590af2e43a8f7e7929442b
Tido Bergmans <24189800+nearestneighbour@users.noreply.github.com>
2024-07-03 12:13:22 +0200

    findexperiments.m now works with multiple conditions

--- Toolbox/Experiments/findexperiments.m
+++ Toolbox/Experiments/findexperiments.m
@@ -1,32 +1,44 @@
-function ids = findexperiments(var,val)
+function ids = findexperiments(varargin)
 %% Find experiments with specific conditions
-% For example experiments where structure_name=='retina'
-% Or where strain=='C57BL/6J'
+% For example based on structure ID, strain or gender
 % 
-% exp_id = findexperiments(var,val)
+% exp_id = findexperiments(Variable, Value, ...)
+% exp_id = findexperiments('all')
 % 
-% Input:
-% var: experiment property, these are the options:
-% id, name, structure_id, strain, transgenic_line, gender, injectionvol,
-% sum, num_voxels, page_url
-% val: value of the property (criterium) - string or number
+% Input: either 'all', or a list of Variable-Value pairs where:
+% Variable is an experiment property, these are the options:
+%   id, name, structure_id, strain, transgenic_line, gender, injectionvol,
+%   sum, num_voxels, page_url
+% Value is the value of the property (criterium) - string or number
 % 
 % Output:
 % ids: Cell array with experiment IDs
 % 
 % Example:
-% index = findexperiment('id','100140756');
-% Returns the experiment with id 100140756
-% 
-% Example:
-% index = findexperiment('gender','M')
-% loadmap('density','density',experiments.id(index))
-% Creates a density network based on experiments in males
+% index = findexperiment('gender','M','transgenic_line','Plxnd-Cre-OG')
+% Returns all experiments using male mice from transgenic line Plxnd-Cre-OG
 
 load('experiments','experiments');
-if ischar(val)
-    index = find(strcmp(experiments.(var),val));
+variables = {'id','name','structure_id','strain','transgenic_line','gender','injectionvol','sum','num_voxels','page_url'};
+
+if nargin == 1
+    if strcmpi(varargin{1}, 'all')
+        index = 1:length(experiments.id);
+    else
+        error(['Unknown parameter ' varargin{1}]);
+    end
 else
-    index = find(experiments.(var)==val);
+    index = true(size(experiments.id));
+    for i = 1:2:length(varargin)
+        if ~ismember(varargin{i}, variables)
+            error(['Unknown variable ' varargin{i}]);
+        end
+        if ischar(varargin{i+1})
+            index = index & strcmp(experiments.(varargin{i}), varargin{i+1});
+        else
+            index = index & (experiments.(varargin{i}) == varargin{i+1});
+        end
+    end
 end
+
 ids = experiments.id(index);
